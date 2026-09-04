0a1c16fd2c9f07bd94b3a00686618dc39d7d79ea
ItzGray <63925441+ItzGray@users.noreply.github.com>
2026-09-04 16:59:48 -0500

    Reroute battle sound

--- replay-embed.js
+++ replay-embed.js
@@ -42,7 +42,7 @@ requireScript('https://play.pokemonshowdown.com/js/lib/ps-polyfill.js');
 requireScript('https://play.pokemonshowdown.com/config/config.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/lib/jquery-1.11.0.min.js');
 requireScript('https://play.pokemonshowdown.com/js/lib/html-sanitizer-minified.js');
-requireScript('https://play.pokemonshowdown.com/js/battle-sound.js');
+requireScript('https://itzgray.github.io/gen-5-trainer-tourney-replay-mods/battle-sound.js');
 requireScript('https://play.pokemonshowdown.com/js/battledata.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/pokedex-mini.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/pokedex-mini-bw.js?a7');
