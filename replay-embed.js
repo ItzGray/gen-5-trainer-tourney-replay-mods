68ca8c54d7093acd2871b0f74ba09392e2c3de55
ItzGray <63925441+ItzGray@users.noreply.github.com>
2026-09-04 16:46:01 -0500

    Hopefully BGM works now

--- replay-embed.js
+++ replay-embed.js
@@ -97,8 +97,6 @@ if(rc2)rc2.innerHTML=rc2.innerHTML;
 if(window.HTMLAudioElement)$('.soundchooser, .startsoundchooser').show();
 this.update();
 this.battle.subscribe(function(state){return _this.update(state);});
-this.battle.scene.bgm.stop();
-BattleSound.deleteBgm(this.battle.scene.bgm);
 this.battle.scene.bgm=new BattleBGM('https://raw.githubusercontent.com/ItzGray/snakewood-showdown-audio/main/bw-n1.mp3',19170,73876);
 },
 $:function(sel){
